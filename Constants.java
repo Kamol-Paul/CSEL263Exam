8f2f1b1e06d2f967fa29c6523d961f7e21368232
Manik Hosen <rahathossenmanik@gmail.com>
2021-12-06 11:41:17 +0600

    we creat a new class for constants and replace it with literals string

---
+++ Constants.java
@@ -0,0 +1,26 @@
+public class Constants {
+    public String SHOW_ALL = "a";
+    public String GET_RANDOM = "r";
+    public String ADD_NEW = "+";
+    public String SEARCH = "?";
+    public String COUNT = "C";
+
+    public String LOADING = "Loading Data...";
+    public String LOADED = "Data Loaded.";
+    public String NO_ARG = "Sorry! You don't enter any argument!";
+    public String WRONG_ARG = "Sorry! You enter wrong ot invalid Argument";
+
+    public String FILE = "students.txt";
+    public String WRONG_OPEN = "Sorry! We can't Open the file.";
+    public String SPLIT_KEY = ",";
+    public String EMPTY_STRING = "";
+
+    public String WORD = " word(s) found";
+    public String FOUND = "We found it!";
+    public String DATE_FORMAT = "dd/mm/yyyy-hh:mm:ss a";
+    public String UPDATE = "\nList last updated on ";
+    Constants() {
+
+    }
+
+}
--- StudentList.java
+++ StudentList.java
@@ -3,58 +3,59 @@ import java.text.*;
 import java.util.*;
 
 public class StudentList {
-    public static void main(String[] args) {
+    public static Constants constants = new Constants();
 
+    public static void main(String[] args) {
 //		Check arguments
         if (args.length < 1) {
-            System.out.println("Sorry! You don't enter any argument!");
-        } else if (args[0].equals("a")) {
-            System.out.println("Loading data ...");
+            System.out.println(constants.NO_ARG);
+        } else if (args[0].equals(constants.SHOW_ALL)) {
+            System.out.println(constants.LOADING);
             BufferedReader str = openFileRead();
 
             if (str == null) return;
 
-            String students[] = getLine(str).split(",");
+            String students[] = getLine(str).split(constants.SPLIT_KEY);
 
             for (String student : students) {
                 System.out.println(student);
             }
 
-            System.out.println("Data Loaded.");
-        } else if (args[0].equals("r")) {
-            System.out.println("Loading data ...");
+            System.out.println(constants.LOADED);
+        } else if (args[0].equals(constants.GET_RANDOM)) {
+            System.out.println(constants.LOADING);
             BufferedReader str = openFileRead();
 
             if (str == null) return;
 
-            String students[] = getLine(str).split(",");
+            String students[] = getLine(str).split(constants.SPLIT_KEY);
 
             Random random = new Random();
             int randomNum = ((random.nextInt() % students.length) + students.length) % students.length;
 
             System.out.println(students[randomNum]);
 
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("+")) {
-            System.out.println("Loading data ...");
+            System.out.println(constants.LOADED);
+        } else if (args[0].contains(constants.ADD_NEW)) {
+            System.out.println(constants.LOADING);
             BufferedWriter str = openFileWrite();
 
             String newStudent = args[0].substring(1);
             Date date = new Date();
-            String df = "dd/mm/yyyy-hh:mm:ss a";
+            String df = constants.DATE_FORMAT;
             DateFormat dateFormat = new SimpleDateFormat(df);
             String updateTime = dateFormat.format(date);
 
             try {
-                str.write("," + newStudent + "\nList last updated on " + updateTime);
+                str.write(constants.SPLIT_KEY + newStudent + constants.UPDATE + updateTime);
                 str.close();
             } catch (Exception E) {
-
+                System.out.println(constants.WRONG_OPEN);
             }
 
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("?")) {
-            System.out.println("Loading data ...");
+            System.out.println(constants.LOADED);
+        } else if (args[0].contains(constants.SEARCH)) {
+            System.out.println(constants.LOADING);
             BufferedReader str = openFileRead();
 
             if (str == null) return;
@@ -64,34 +65,34 @@ public class StudentList {
 
             for (int idx = 0; idx < students.length; idx++) {
                 if (students[idx].equals(tergetStudent)) {
-                    System.out.println("We found it!");
+                    System.out.println(constants.FOUND);
                     break;
                 }
             }
 
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("c")) {
-            System.out.println("Loading data ...");
+            System.out.println(constants.LOADED);
+        } else if (args[0].contains(constants.COUNT)) {
+            System.out.println(constants.LOADING);
             BufferedReader str = openFileRead();
 
             if (str == null) return;
 
-            String students[] = getLine(str).split(",");
+            String students[] = getLine(str).split(constants.SPLIT_KEY);
             int count = students.length;
-            System.out.println(count + " word(s) found");
-            System.out.println("Data Loaded.");
-            
+            System.out.println(count + constants.WORD);
+            System.out.println(constants.LOADED);
+
         } else {
-            System.out.println("Sorry! You enter wrong ot invalid Argument");
+            System.out.println(constants.WRONG_ARG);
         }
     }
 
     private static BufferedReader openFileRead() {
         BufferedReader str = null;
         try {
-            str = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+            str = new BufferedReader(new InputStreamReader(new FileInputStream(constants.FILE)));
         } catch (Exception e) {
-            System.out.println("Sorry! We can't Open the file");
+            System.out.println(constants.WRONG_OPEN);
         }
         return str;
     }
@@ -99,9 +100,9 @@ public class StudentList {
     private static BufferedWriter openFileWrite() {
         BufferedWriter str = null;
         try {
-            str = new BufferedWriter(new FileWriter("students.txt", true));
+            str = new BufferedWriter(new FileWriter(constants.FILE, true));
         } catch (Exception e) {
-            System.out.println("Sorry! We can't Open the file");
+            System.out.println(constants.WRONG_OPEN);
         }
         return str;
     }
@@ -112,6 +113,6 @@ public class StudentList {
         } catch (Exception E) {
 
         }
-        return "";
+        return constants.EMPTY_STRING;
     }
 }
